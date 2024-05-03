e44ec684959112707b54453cea88669643fe6035
Bl1tzed <dns-2001@mail.ru>
2024-05-03 20:08:37 +0600

    feat: mobileMenu in header

--- src/features/navigation/ui/navigation.tsx
+++ src/features/navigation/ui/navigation.tsx
@@ -1,13 +1,30 @@
 import { NAVIGATION_CONTENT } from "@shared/consts";
 import { NavLink } from "react-router-dom";
+import { cva, type VariantProps } from "class-variance-authority";
 import clsx from "clsx";
 
 import s from "./navigation.module.scss";
 
-export const Navigation = () => {
+const navigationVariants = cva(s.button, {
+  variants: {
+    variant: {
+      header: s.header,
+      mobileMenu: s.mobileMenu,
+    },
+  },
+  defaultVariants: {
+    variant: "header",
+  },
+});
+
+export const Navigation = (
+  props: VariantProps<typeof navigationVariants> & { className?: string }
+) => {
+  const { className, variant } = props;
+
   return (
-    <nav>
-      <ul className={s.navigationList}>
+    <nav className={className}>
+      <ul className={clsx(navigationVariants({ variant }))}>
         {NAVIGATION_CONTENT.map((item) => (
           <li key={item.title} className={clsx(s.navigationItem)}>
             <NavLink
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,23 +1,74 @@
-import { Navigation } from "@features/navigation";
+import { useEffect, useState } from "react";
+import { useLocation, NavLink } from "react-router-dom";
+import { easeInOut, motion } from "framer-motion";
+import { RemoveScroll } from "react-remove-scroll";
 import { Button } from "@shared/ui/button";
-import { NavLink } from "react-router-dom";
+import { Navigation } from "@features/navigation";
+import clsx from "clsx";
 
 import s from "./header.module.scss";
 
 export const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setMenuIsOpen(false);
+  }, [location]);
+
+  const variants = {
+    closed: {
+      x: "100%",
+      transition: {
+        ease: easeInOut,
+        duration: 1,
+      },
+    },
+    open: {
+      x: "0%",
+      transition: {
+        ease: easeInOut,
+        duration: 1,
+      },
+    },
+  };
+
   return (
-    <header className={s.header}>
-      <div className={s.headerInner}>
-        <NavLink to="/">
-          <div className={s.logo}>Bibiblog</div>
-        </NavLink>
-        <Navigation />
-        <div className={s.headerButton}>
-          <Button variant="primary" size="small">
+    <>
+      <header className={s.header}>
+        <div className={s.headerInner}>
+          <NavLink to="/">
+            <div className={s.logo}>Bibiblog</div>
+          </NavLink>
+          <Navigation className={s.navigationMenu} />
+          <div className={s.headerButton}>
+            <Button variant="primary" size="small">
+              Contact Us
+            </Button>
+          </div>
+          <button
+            className={clsx(s.navButton, menuIsOpen && s.open)}
+            onClick={() => setMenuIsOpen((prev) => !prev)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </header>
+      <RemoveScroll enabled={menuIsOpen} removeScrollBar={false}>
+        <motion.aside
+          initial={false}
+          variants={variants}
+          animate={menuIsOpen ? "open" : "closed"}
+          className={clsx(s.mobileMenu, menuIsOpen && s.open)}
+        >
+          <Navigation variant={"mobileMenu"} />
+          <Button variant="primary" size="big" className={s.mobileMenuButton}>
             Contact Us
           </Button>
-        </div>
-      </div>
-    </header>
+        </motion.aside>
+      </RemoveScroll>
+    </>
   );
 };
